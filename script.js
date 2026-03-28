1af2c4e57bbaa32af84fae6a1016d7b94cacd368
Guta Desalegn <gutageda27@gmail.com>
2026-03-28 14:56:53 +0300

    Update index.html

--- script.js
+++ script.js
@@ -70,6 +70,15 @@ form.addEventListener("submit", async (e) => {
     submitBtn.disabled = false;
   }
 });
+const sideMenu = document.getElementById("sideMenu");
+
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-16rem";
+}
 
 
 // ===== TOAST NOTIFICATION =====
